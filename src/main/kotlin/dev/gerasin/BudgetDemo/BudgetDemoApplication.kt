eb18de45f29a45169a7a268c5b1ed9133ac11635
Ivan Gerasin <ivan@gerasin.dev>
2021-04-03 11:07:31 +0700

    init

---
+++ src/main/kotlin/dev/gerasin/BudgetDemo/BudgetDemoApplication.kt
@@ -0,0 +1,11 @@
+package dev.gerasin.BudgetDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BudgetDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<BudgetDemoApplication>(*args)
+}
---
+++ src/test/kotlin/dev/gerasin/BudgetDemo/BudgetDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.gerasin.BudgetDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
